cf0e73769b3e3a2583f2ede8a2fd801a61d9dfd8
SamuelMaiko <sammymaiko721@gmail.com>
2025-04-04 19:13:01 +0300

    final code

--- custom-addons/task-management/models/task.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Task(models.Model):
-    _name="task.task"
-    _description="Task storing"
-
-    name=fields.Char(string="Name", required=True)
-    is_completed = fields.Boolean(string="Is Completed", default=False)
