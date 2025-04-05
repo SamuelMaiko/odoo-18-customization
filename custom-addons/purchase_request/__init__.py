c044fbb722545e357d09a1e89527d7ff63af6ed1
SamuelMaiko <sammymaiko721@gmail.com>
2025-04-06 00:05:47 +0300

    gitignore added to remove odoo folder

--- custom-addons/purchase_request/__init__.py
+++ custom-addons/purchase_request/__init__.py
@@ -1,4 +1,4 @@
 from . import models
 
 # to do
-# 1. handle the cancel button after approve (delete rfq) and reject (just state)
+# 1. handle the cancel button after approve (delete rfq) and reject (just state), cancel button in the requests form view
